101b9fdeddc97dc290d714b4e8d7de2ae9cae9e3
Oleg Eterevsky <oleg@eterevsky.com>
2013-08-18 09:52:21 +0400

    Chang main program output.

--- gamerank.py
+++ gamerank.py
@@ -9,13 +9,23 @@ def main():
     players = db.load_players()
     optimizer = Optimizer(disp=True, rating_reg=1E-4, time_delta=1E-3, games_delta=1E-3)
     optimizer.load_games(results)
-    ratings, _, _ = optimizer.run()
+    ratings, f, _ = optimizer.run()
+    by_rating = []
     for iplayer, rating in ratings.items():
         print(players[iplayer])
+        mr = 0
         for date, r in sorted(rating.items()):
             date = datetime.date.fromtimestamp(date * 24 * 3600)
             print(date.isoformat(), r)
+            if r > mr:
+                mr = r
+        by_rating.append((mr, players[iplayer]))
+
         print()
+    print(f)
+    print(f.calc(200) - f.calc(-200))
+    print(list(sorted(by_rating))[-10:])
+
 
 if __name__ == '__main__':
     main()
