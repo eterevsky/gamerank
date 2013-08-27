7e125c17f817c562fc5150932efd5e4e0105d71f
Oleg Eterevsky <olege@google.com>
2013-08-27 14:22:34 +0400

    Import several PGN files at once.

--- import_pgn.py
+++ import_pgn.py
@@ -3,20 +3,22 @@ import sys
 from game import PGNParser
 from gamesdb import DataBase, DuplicateGameError
 
-if len(sys.argv) != 2:
-    print('Usage:\npython3 import_pgn.py <pgn file>')
+if len(sys.argv) < 2:
+    print('Usage:\npython3 import_pgn.py <pgn files>')
     exit(0)
 
 db = DataBase('games.db')
-parser = PGNParser(open(sys.argv[1], encoding='iso-8859-1'))
-errors = []
-for game in parser.parse():
-    try:
-        db.add_game(game)
-    except DuplicateGameError as error:
-        errors.append(error)
-    print(game)
-db.commit()
+
+for filename in sys.argv[1:]:
+    parser = PGNParser(open(filename, encoding='iso-8859-1'))
+    errors = []
+    for game in parser.parse():
+        try:
+            db.add_game(game)
+        except DuplicateGameError as error:
+            errors.append(error)
+        print(game)
+    db.commit()
 
 if len(errors):
     print('Errors:')
